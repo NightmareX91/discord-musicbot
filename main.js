8c8daddc7a952c21a8978c4dab7d1c8f06f0c223
meanwhile <nightmarex91@gmail.com>
2016-03-19 15:54:51 +0000

    Added example server.json and a check for server.json

--- main.js
+++ main.js
@@ -13,3 +13,17 @@ catch (e) {
     console.log("Please create an auth.json file using the auth.json.example file from the github repository!");
     process.exit();
 }
+
+try {
+    var serverDetails = require("./server.json");
+}
+catch (e) {
+    console.log("Please create a server.json file using the server.json.example file from the github repository!");
+    process.exit();
+}
+
+var bot = new discord.Client();
+
+bot.on("ready", function() {
+    console.log("Ready to begin playing slick beats!");
+})
